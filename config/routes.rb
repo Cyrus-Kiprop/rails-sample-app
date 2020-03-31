e37d8f7a0961d59bce3329845483bd8bbb5b28ad
kip <cyruskiprop254@gmail.com>
2020-04-01 00:01:59 +0300

    Finish layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'foo/bar'
-  get 'foo/baz'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  get '/signup', to: 'users#new'
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help', as: 'helf'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 end
